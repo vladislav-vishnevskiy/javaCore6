4f9901924e9d64521e247d9f86f9033061677ac8
vladislav-vishnevskiy <vladislav.vishnevskiy@gmail.com>
2017-01-25 20:29:07 +0200

    Homework #1, Module 1, JavaCore6

---
+++ FirstClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Vladislav on 25.01.2017.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
---
+++ SecondClass.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Vladislav on 25.01.2017.
+ */
+public class SecondClass {
+    public static void main(String[] args) {
+        System.out.println("Мой первый класс на Java");
+    }
+}
