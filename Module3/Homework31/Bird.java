69bb536a422963e7788ca8b140279915dbba0c0a
vladislav-vishnevskiy <vladislav.vishnevskiy@gmail.com>
2017-02-11 22:16:11 +0200

    Edited 3.1

--- Module3/Homework31/Bird.java
+++ Module3/Homework31/Bird.java
@@ -5,11 +5,7 @@ package Module3.Homework31;
  */
 public class Bird {
     public static String sing() {
-        String firstRow = "I am walking\n";
-        String secondRow = "I am flying\n";
-        String thirdRow = "I am singing\n";
-        String fourthRow = "I am Bird!!!";
-        String song = firstRow+secondRow+thirdRow+fourthRow;
+        String song = "I am walking!\nI am flying!\nI am singing!\nI am a Bird!";
         return song;
     }
 
